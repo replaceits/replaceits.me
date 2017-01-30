9f36a1d770b409d5038403dc8f42da4bc0297b50
replaceits <sharpcut2231@gmail.com>
2017-01-29 23:14:17 -0500

    Slight additions

---
+++ js/console.js
@@ -0,0 +1,6 @@
+console.log("______ ___________ _       ___  _____  _____ _____ _____ _____   ");
+console.log("| ___ \\  ___| ___ \\ |     / _ \\/  __ \\|  ___|_   _|_   _/  ___|          Sidney Williams");
+console.log("| |_/ / |__ | |_/ / |    / /_\\ \\ /  \\/| |__   | |   | | \\ `--.   Github: https://github.com/replaceits");
+console.log("|    /|  __||  __/| |    |  _  | |    |  __|  | |   | |  `--. \\  Resume: https://replaceits.com/resume.pdf");
+console.log("| |\\ \\| |___| |   | |____| | | | \\__/\\| |___ _| |_  | | /\\__/ /  E-Mail: sidwil0790@students.ecpi.edu");
+console.log("\\_| \\_\\____/\\_|   \\_____/\\_| |_/\\____/\\____/ \\___/  \\_/ \\____/   Phone#: 1(434)260-0420");
