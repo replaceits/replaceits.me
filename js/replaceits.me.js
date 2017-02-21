264cbfbf1a221bdb9fdc8bc5c4e403801e4d725c
replaceits <sharpcut2231@gmail.com>
2017-02-21 07:08:50 -0500

    Media queries for desktop.

--- js/replaceits.me.js
+++ js/replaceits.me.js
@@ -12,16 +12,19 @@ $(document).ready(function(){
         });
     });
 
-    //Check if mobile device
+    // Check if mobile device
     if(window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 480px)")){
-        //This will prevent the page from jumping when the address bar hides
+        // This will prevent the page from jumping when the address bar hides
+        // by setting the content pages height to it's own height in px on document
+        // load rather than having it in 'vh' units.
         $('.content-page.full').css('height',$('.content-page.full').height() + 'px').css('min-height',$('.content-page.full').height() + 'px');
 
         // Prevent image links from being pressed until hovering over the image.
         // This prevents accidental clicking of the image controls on mobile,
         // when touching the image. 
         $('.content-project-image-container').hover(function(){
-            // On mouse enter enable the link
+            // On mouse enter enable the link (short delay to prevent it from 
+            // enabling during a users first press in the box).
             setTimeout(function(anchor){
                 anchor.children('.content-project-image-controls').children('a').unbind('click').click(function(event){
                     return true;
@@ -36,7 +39,5 @@ $(document).ready(function(){
             // Disable all image links on first load
             return false;
         });
-
     }
-
 });
