e0fdcef9dd22860e66e0164a26aced58e59a81dd
replaceits <sharpcut2231@gmail.com>
2017-02-21 02:56:04 -0500

    Add mobile support

--- js/replaceits.me.js
+++ js/replaceits.me.js
@@ -1,7 +1,34 @@
+/// <reference path="../typings/index.d.ts" />
+
 $(document).ready(function(){
+
+    // Smooth scrolling down when the chevron is pressed.
     $('.fa-chevron-down').click(function(){
         $('html, body').animate({
             scrollTop: $("#Skills").offset().top
-        }, {duration: 1250, easing: 'swing'});
+        }, {
+            duration: 1250,
+            easing: 'swing'
+        });
+    });
+    
+    // Prevent image links from being pressed until hovering over the image.
+    // This prevents accidental clicking of the image controls on mobile,
+    // when touching the image. 
+    $('.content-project-image-container').hover(function(){
+        // On mouse enter enable the link
+        setTimeout(function(anchor){
+            anchor.children('.content-project-image-controls').children('a').unbind('click').click(function(event){
+                return true;
+            });
+        },50,$(this));
+    },function(){
+        // On mouse leave disable the link
+        $(this).children('.content-project-image-controls').children('a').unbind('click').click(function(event){
+            return false;
+        });
+    }).children('.content-project-image-controls').children('a').unbind('click').click(function(event){
+        // Disable all image links on first load
+        return false;
     });
 });
