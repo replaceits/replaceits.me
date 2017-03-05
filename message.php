59c7287f84a691d466e4d06b438a589ab471650b
replaceits <sharpcut2231@gmail.com>
2017-03-04 22:28:32 -0500

    test

--- message.php
+++ message.php
@@ -107,7 +107,7 @@
         'Content-Type: text/plain; charset=utf-8' . "\r\n" .
         'X-Priority: 1' . "\r\n" .
         'X-Mailer: PHP/' . phpversion() . "\r\n"
-    ));
+    ) ? "Sent!" : "Not Sent :(");
 
     http_response_code(200);
 
